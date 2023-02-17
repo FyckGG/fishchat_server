deb143d21d45d24ba9ad4a5666f98c4a74b96a4b
DmitryFaber <mistertwister698@gmail.com>
2023-02-17 20:56:39 +0700

    small update for authenticateToken

--- middleware/authenticateToken.ts
+++ middleware/authenticateToken.ts
@@ -1,19 +1,26 @@
+import express from "express";
 import jwt from "jsonwebtoken";
 import UserError from "../expections/UserError";
 
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.autorization;
+const authenticateToken = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const authHeader = req.headers.authorization as string;
 
   if (authHeader) {
     const token = authHeader.split(" ")[1];
+
     jwt.verify(
       token,
       process.env.ACCES_TOKEN_SECRET_KEY as jwt.Secret,
-      (err, user) => {
+      //(err, user) => {
+      (err) => {
         if (err) {
           throw UserError.UnautorizedError();
         }
-        req.user = user;
+        //req.user = user;
         next();
       }
     );
@@ -21,3 +28,5 @@ const authenticateToken = (req, res, next) => {
     throw UserError.UnautorizedError();
   }
 };
+
+export default authenticateToken;
