be8e62571c479a6bf7c153b621dc541235bd7102
DmitryFaber <mistertwister698@gmail.com>
2023-02-22 18:23:47 +0700

    add websocket server

---
+++ websocket_server.ts
@@ -0,0 +1,28 @@
+import WebSocket from "ws";
+import UsersSearchService from "./services/UsersSearchService";
+
+const createWss = () => {
+  const WSS_PORT = process.env.WSS_PORT || 5001;
+  const wss = new WebSocket.Server({ port: WSS_PORT as number }, () => {
+    console.log(`websocket started on port ${WSS_PORT}`);
+  });
+
+  wss.on(`connection`, (ws) => {
+    ws.on(`message`, async (message) => {
+      const json_message = JSON.parse(message.toString());
+      console.log(JSON.parse(message.toString()));
+      switch (json_message.type) {
+        case process.env.VITE_REACT_APP_WSS_USERFIND_TYPE: {
+          const responce = await UsersSearchService.searchByName(
+            json_message.message
+          );
+          ws.send(responce.toString());
+        }
+      }
+
+      ws.send(message.toString());
+    });
+  });
+};
+
+export default createWss;
