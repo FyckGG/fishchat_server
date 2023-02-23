da65cd13a5478c8f47876bf5bc573f3285602454
DmitryFaber <mistertwister698@gmail.com>
2023-02-23 15:40:21 +0700

    renamed userSearch service

--- services/SearchService.ts
+++ services/SearchService.ts
@@ -2,13 +2,19 @@ import { Types } from "mongoose";
 import FilterService from "./FilterService";
 import UserStatusService from "./UserStatusService";
 import UserDtoForSearch from "../dtos/UserDtoForSearch";
+import MessageDto from "../dtos/MessageDto";
+import MessageSearchService from "./MessageSearchService";
 
 class UserSearchService {
-  async searchByName(searching_user: Types.ObjectId, search_string: string) {
+  async searchUsersByName(
+    searching_user: Types.ObjectId,
+    search_string: string
+  ) {
     const filtered_users_list = await FilterService.filterUsersByName(
       search_string
     );
     const user_dto_for_search: UserDtoForSearch[] = [];
+    const messages_for_search: MessageDto[] = [];
     await Promise.all(
       filtered_users_list.map(async (user) => {
         const user_status = await UserStatusService.getUsersRelationships(
@@ -19,10 +25,21 @@ class UserSearchService {
         user_dto_for_search.push({
           ...new UserDtoForSearch(user, user_status),
         });
+
+        const user_message =
+          await MessageSearchService.searchLastMessageByUsername(
+            searching_user,
+            user._id
+          );
+        if (user_message)
+          messages_for_search.push({ ...new MessageDto(user_message) });
       })
     );
 
-    return { ...user_dto_for_search };
+    return {
+      users: { ...user_dto_for_search },
+      dialogs: { ...messages_for_search },
+    };
   }
 }
 
