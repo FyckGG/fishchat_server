d77bf1bfc1e7ef4f9afd25b1e2cf141b89fe35b7
DmitryFaber <mistertwister698@gmail.com>
2023-02-23 13:31:09 +0700

    add textConstants

---
+++ textConstants/userRelationships.ts
@@ -0,0 +1,10 @@
+const enum RouteNames {
+  FRIEND = "friends",
+  SUBSCRIBER_SOURCE = "subscriber_source",
+  SUBSCRIBER_TARGET = "subscriber_target",
+  BLOCKED_SOURCE = "blocked_source",
+  BLOCKED_TARGET = "blocked_target",
+  DEFAULT = "default",
+}
+
+export default RouteNames;
