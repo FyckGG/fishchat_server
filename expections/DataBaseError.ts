142d9aa507b44c9837e637560143d3c62c26882e
DmitryFaber <mistertwister698@gmail.com>
2023-02-28 16:09:46 +0700

    add new error type in DataBaseError

--- expections/DataBaseError.ts
+++ expections/DataBaseError.ts
@@ -7,6 +7,17 @@ export default class DataBaseError extends Error {
     super(message);
   }
 
+  static AddingAgainError() {
+    return new DataBaseError(400, "Already been added.");
+  }
+
+  static CancelActionError() {
+    return new DataBaseError(
+      404,
+      "it is not possible to cancel the operation because it was not performed earlier."
+    );
+  }
+
   static DocumentNotFound(message: string, errors = []) {
     return new DataBaseError(404, message, errors);
   }
