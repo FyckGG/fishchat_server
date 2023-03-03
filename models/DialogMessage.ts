a9a7d2bff38704732241d6778b881747f9c40952
DmitryFaber <mistertwister698@gmail.com>
2023-03-03 18:44:47 +0700

    update DialogMessage modeul

--- models/DialogMessage.ts
+++ models/DialogMessage.ts
@@ -4,7 +4,7 @@ const DialogMessage = new Schema({
   source_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
   target_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
   message_text: { type: String, required: true },
-  is_message_read: { type: Boolean, required: true },
+  is_message_read: { type: Boolean, required: true, default: false },
   message_date: { type: Date, default: Date.now, required: true },
 });
 
